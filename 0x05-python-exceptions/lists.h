a1c0349c6d02191a0a85a953003817216e2f9740
charlesnzisa <charlesnzisa1993@gmail.com>
2023-01-23 08:26:38 -0800

    task 7 commit

--- 0x05-python-exceptions/lists.h
+++ 0x05-python-exceptions/lists.h
@@ -8,5 +8,10 @@ def safe_print_division(a, b):
 def list_division(my_list_1, my_list_2, list_length):
 def raise_exception():
 def raise_exception_msg(message=""):
-
+def safe_print_integer_err(value):
+def safe_function(fct, *args):
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
+def magic_calculation(a, b):
 #endif
