6c3fd32225c5d27e8e559c3c0ace947d45eed61f
charlesnzisa <charlesnzisa1993@gmail.com>
2023-01-29 10:50:34 -0800

    Task 6 commit

--- 0x07-python-test_driven_development/lists.h
+++ 0x07-python-test_driven_development/lists.h
@@ -7,5 +7,8 @@ def say_my_name(first_name, last_name=""):
 def print_square(size):
 def text_indentation(text):
 def max_integer(list=[]):
+def matrix_mul(m_a, m_b):
+def lazy_matrix_mul(m_a, m_b):
+void print_python_string(PyObject *p);
 
 #endif
