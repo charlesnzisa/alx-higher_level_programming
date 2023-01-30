6fc30dbf1881f237fabb70566d18b0d2384528ae
charlesnzisa <charlesnzisa1993@gmail.com>
2023-01-30 06:17:01 -0800

    task 7 commit

--- 0x06-python-classes/lists.h
+++ 0x06-python-classes/lists.h
@@ -9,5 +9,11 @@ def my_print(self):
 def position(self):
 def position(self, value):
 def __init__(self, size=0, position=(0, 0)):
+def data(self):
+def data(self, value):
+def next_node(self):
+def next_node(self, value)
+def __init__(self, data, next_node=None):
+def sorted_insert(self, value):
 
 #endif
