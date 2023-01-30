427c6d2380832dcd0ca3da5a6e647b7aaa162043
charlesnzisa <charlesnzisa1993@gmail.com>
2023-01-30 04:57:23 -0800

    first commit

---
+++ 0x08-python-more_classes/lists.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+def width(self):
+def width(self, value):
+def height(self):
+def height(self, value):
+def __init__(self, width=0, height=0):
+def area(self):
+def perimeter(self):
+
+#endif
