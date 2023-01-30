b87e813f57630261c51845babc8197cb3da9ce69
charlesnzisa <charlesnzisa1993@gmail.com>
2023-01-30 05:56:01 -0800

    TASK 0 commit

---
+++ 0x06-python-classes/lists.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+def __init__(self, size=0):
+def area(self):
+def size(self):
+def size(self, value):
+def my_print(self):
+def position(self):
+def position(self, value):
+def __init__(self, size=0, position=(0, 0)):
+
+#endif
