c7bd759bd30d94770538e0ae9b658b4d09cb8966
charlesnzisa <charlesnzisa1993@gmail.com>
2023-02-06 10:38:50 -0800

    first commit

---
+++ 0x0A-python-inheritance/lists.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+def lookup(obj):
+def is_same_class(obj, a_class):
+def is_kind_of_class(obj, a_class):
+def inherits_from(obj, a_class):
+def area(self):
+def __init__(self, width, height):
+def __init__(self, size):
+def __init__(self, size):
+
+#endif
