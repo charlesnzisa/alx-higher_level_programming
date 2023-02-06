88f9b4a66093bc4f63f19e24a3adc9b834b00093
charlesnzisa <charlesnzisa1993@gmail.com>
2023-02-06 12:56:55 -0800

    lists commit

--- 0x0A-python-inheritance/lists.h
+++ 0x0A-python-inheritance/lists.h
@@ -5,9 +5,9 @@ def lookup(obj):
 def is_same_class(obj, a_class):
 def is_kind_of_class(obj, a_class):
 def inherits_from(obj, a_class):
-def area(self):
+size: def __init__(self, size):
 def __init__(self, width, height):
-def __init__(self, size):
-def __init__(self, size):
+def area(self):
+def print_sorted(self):
 
 #endif
