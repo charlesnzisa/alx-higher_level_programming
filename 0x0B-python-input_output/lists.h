e2611d018e4bbdb56d6c344244aedbbf1b1f3475
charlesnzisa <charlesnzisa1993@gmail.com>
2023-02-07 00:01:32 -0800

    first commit

---
+++ 0x0B-python-input_output/lists.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+def read_file(filename=""):
+def write_file(filename="", text=""):
+def append_write(filename="", text=""):
+def to_json_string(my_obj):
+def from_json_string(my_str):
+def save_to_json_file(my_obj, filename):
+def load_from_json_file(filename):
+def class_to_json(obj):
+def __init__(self, first_name, last_name, age):
+def to_json(self):
+def to_json(self, attrs=None):
+def reload_from_json(self, json):
+def pascal_triangle(n):
+
+#endif
